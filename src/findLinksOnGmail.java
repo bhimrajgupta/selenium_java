3923f4de8ab311952c9de834a7af32fc18db06ce
bhimrajgupta <bhimrajgupta@yahoo.com>
2018-07-20 07:48:02 +0530

    Test: Find links on Gmail

--- src/findLinksOnGmail.java
+++ src/findLinksOnGmail.java
@@ -3,23 +3,32 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GmailLinksTest {
+class findLinksOnGmail {
 
     @Test
     public void testFindLinks(){
         //Initialize the browser driver
         WebDriver driver = new FirefoxDriver();
+
         driver.get("www.gmail.com");
+
         //Get all the links displayed on the page
         List<WebElement> links = driver.findElements(By.tagName("a"));
+
         //Verify there are 4 links displayed on the page
         assertEquals(4, links.size());
+
+        //Print target for each links
+        for (WebElement link : links){
+            System.out.println(link.getAttribute("href"));
+        }
     }
 
 }
